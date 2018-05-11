11a10229bcb3a9e8ebe5b66fff586b2b7d42fdb2
479954871 <49954871@qq.com>
2018-05-12 00:35:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a47995/experimentone/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.a47995.experimentone;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        }
+}
